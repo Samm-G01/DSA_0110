1a5611f901160029e642ec4b03df2b4b8ff9636c
Samm-G01 <152312020+Samm-G01@users.noreply.github.com>
2026-08-12 20:03:53 +0530

    Implement Pacific Atlantic Water Flow solution

---
+++ leetcode_417.java
@@ -0,0 +1,45 @@
+class Solution {
+    int m, n;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        m = heights.length;
+        n = heights[0].length;
+
+        boolean[][] pac = new boolean[m][n];
+        boolean[][] atl = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pac, i, 0);
+            dfs(heights, atl, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pac, 0, j);
+            dfs(heights, atl, m - 1, j);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pac[i][j] && atl[i][j])
+                    ans.add(Arrays.asList(i, j));
+            }
+        }
+        return ans;
+    }
+
+    void dfs(int[][] h, boolean[][] vis, int r, int c) {
+        vis[r][c] = true;
+
+        for (int[] d : dirs) {
+            int nr = r + d[0], nc = c + d[1];
+
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                !vis[nr][nc] && h[nr][nc] >= h[r][c]) {
+                dfs(h, vis, nr, nc);
+            }
+        }
+    }
+}
